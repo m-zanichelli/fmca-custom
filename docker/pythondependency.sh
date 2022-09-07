c8db47d4d9f8eb1490e6919c10885223afef49fc
daveb-dev <davide.baroli1@gmail.com>
2022-09-07 18:47:31 +0200

    Update pythondependency.sh

--- docker/pythondependency.sh
+++ docker/pythondependency.sh
@@ -1,4 +1,4 @@
-apk add curl gfortran openblas && \
+apk add curl  python3-dev   build-base gfortran openblas-dev && \
 cd /tmp && \
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
 python3 get-pip.py && \
